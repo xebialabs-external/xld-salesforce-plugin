041bb789baf038191b19bf91862d3a54683087ae
Dan Robinson <drobinson@xebialabs.com>
2019-11-09 15:27:12 -0700

    Initial commit

---
+++ src/main/jython/sfdc/metadata_package/validate.py
@@ -0,0 +1,22 @@
+#
+# Copyright 2019 XEBIALABS
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+from sfdc.metadata_package import SalesForceClient
+
+def validate_package(org_ci, deployed):
+    print "Starting validation"
+    client = SalesForceClient.new_instance(org_ci)
+    client.validate_package(deployed.file.path)
+    print "Done"
+
+if __name__ == '__main__' or __name__ == '__builtin__':
+    container = deployed.container
+    validate_package(container, deployed)
+
